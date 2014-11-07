82e70acfd51cac16e5b6861a5b947f7aaf506725
ninjabi <famf3f@mail.umkc.edu>
2014-11-07 12:41:14 -0600

    Checking to make sure I can change the branch!

---
+++ Program 6 Algorithm.py
@@ -0,0 +1,24 @@
+1"""
+Fatima Mohamed
+COMP - SCI 101
+Algorithm #6
+Due: 11/09/14
+
+Problem:
+    - Creating a program that compares multiple images and replaces portions that are not similar to other parts. Useful
+    when removing pesky tourists that just don't know how to get out of an image!
+
+Algorithm:
+    1.
+    2.
+    3.
+    4.
+    5.
+    6.
+    7.
+    8.
+    9.
+    10.
+
+
+"""
