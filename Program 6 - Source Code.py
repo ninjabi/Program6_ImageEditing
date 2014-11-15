15425471b8fadaec10757d3875da4481eed132a0
ninjabi <famf3f@mail.umkc.edu>
2014-11-14 19:51:55 -0600

    05- Iterating through lines of files! (NEED TO WORK)

--- Program 6 - Source Code.py
+++ Program 6 - Source Code.py
@@ -13,12 +13,14 @@ def input_directory():
         except ValueError:
             print("Please enter a valid input [1/2]")
             continue
-    if working_directory ==2:
+    if working_directory ==1:
+        Files = os.listdir(os.getcwd())
+    if working_directory == 2:
         while True:
             try:
                 global input_directory
                 input_directory = input("Enter the directory of the source images: ")
-                os.chdir(input_directory)
+                os.chdir(input_directory) #changes working directory to users input
             except FileNotFoundError:
                 print("The directory {} is not found.".format(input_directory))
                 continue
@@ -61,16 +63,26 @@ def new_file():
         output_file_name = input("You cannot enter blank as a file name. Please try again: ")
     output_file = open(output_file_name, "a")
 def iterate_files():
-    global Files, file_list
-    file_list = []
+    global Files, file_list, PPMS, num_lines
     Files = os.listdir(os.getcwd())
-    PPMS = [f for f in Files if f.lower().endswith('.ppm')]
-    print("\nBeginning to iterate through", len(PPMS), "files...")
+    PPMS = [open(f, "r") for f in Files if f.lower().endswith('.ppm')] #opens all files that end with .ppm
+    print("\nBeginning to iterate through the files...")
+    for file in PPMS:
+        num_lines = (sum(1 for x in file))
+        print(num_lines)
+        break
 
-
-
+def median_filter():
+    for file in PPMS:   #iterate over files in PPMS (the list of the files)
+        for line in file:    #iterate over line within each file
+            for j in range(1, num_lines):
+                CurrentLine = line[1]
+                print(CurrentLine)
+                #line = int(line) #convert each line to an integer
+            #print(CurrentLine)
 
 
 input_directory()
 new_file()
 iterate_files()
+median_filter()
